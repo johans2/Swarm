82cac034d36c62dcf216c3cb43f77a4008b5a293
Johan Svensson <js@resolutiongames.com>
2020-02-14 17:00:55 +0100

    Added the project.

---
+++ Assets/ComputeShaders/Swarm.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swarm : MonoBehaviour
+{
+    public ComputeShader swarmComputShader;
+    public int numSwarmers = 100000;
+
+    private int kernel;
+
+    void Start()
+    {
+        kernel = swarmComputShader.FindKernel("SwarmMain");
+
+
+    }
+
+
+    void Update()
+    {
+        swarmComputShader.Dispatch(1, 1, 1, 1);
+
+
+    }
+}

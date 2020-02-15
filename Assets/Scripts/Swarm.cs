f7618bb6df11970917625f5aea0b18de960dc74c
johan_home <johansvensson@outlook.com>
2020-02-15 13:31:41 +0100

    World nodes visualized.

---
+++ Assets/Scripts/Swarm.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swarm : MonoBehaviour
+{
+    struct WorldNode
+    {
+        Vector3 position;
+        float occupied;
+    }
+
+    public Vector3Int worldSize = new Vector3Int(10, 10, 10);
+    public Material worldMaterial;
+    public ComputeShader swarmComputShader;
+    public int numSwarmers = 100000;
+
+    private int kernel;
+    private ComputeBuffer worldBuffer;
+    private int NumWorldNodes => worldSize.x * worldSize.y * worldSize.z;
+
+    private WorldNode[] debugWorldNodes;
+
+    void Start()
+    {
+        kernel = swarmComputShader.FindKernel("SwarmMain");
+        debugWorldNodes = new WorldNode[NumWorldNodes];
+
+        WorldNode[] nodes = new WorldNode[NumWorldNodes];
+
+        worldBuffer = new ComputeBuffer(NumWorldNodes, 16);
+        worldBuffer.SetData(nodes);
+        
+        swarmComputShader.SetInt("width", worldSize.x);
+        swarmComputShader.SetInt("height", worldSize.y);
+        swarmComputShader.SetInt("depth", worldSize.z);
+        swarmComputShader.SetBuffer(kernel, "world", worldBuffer);
+        worldMaterial.SetBuffer("world", worldBuffer);
+
+    }
+
+
+    void Update()
+    {
+        swarmComputShader.Dispatch(kernel, 10, 10, 10);
+
+        //worldBuffer.GetData(reNodes); Buffer filled!
+        
+    }
+
+    private void OnRenderObject()
+    {
+        worldMaterial.SetPass(0);
+        
+        Graphics.DrawProceduralNow(MeshTopology.Points, 1, NumWorldNodes);
+    }
+
+    private void OnDestroy()
+    {
+        worldBuffer.Dispose();
+    }
+}
---
+++ Assets/Shaders/worldPoints.shader
@@ -0,0 +1,57 @@
+﻿Shader "Unlit/worldPoints"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 5.0
+
+            #include "UnityCG.cginc"
+
+			struct WorldNode {
+				float3 position;	// = 12
+				float occupied;		// = 4
+			};
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+            };
+
+			half4 _Color;
+			StructuredBuffer<WorldNode> world;
+
+
+            v2f vert (appdata v, uint instance_id : SV_InstanceID)
+            {
+                v2f output = (v2f)0;
+
+				WorldNode node = world[instance_id];
+				output.vertex = UnityObjectToClipPos(node.position);
+				
+                return output;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return _Color;
+            }
+            ENDCG
+        }
+    }
+}
